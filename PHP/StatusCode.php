db974fed227546647117a728525ed06c2bb94293
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2022-01-21 11:54:11 +0100

    Create StatusCode.php

---
+++ PHP/StatusCode.php
@@ -0,0 +1,98 @@
+<?php
+    class StatusCode {
+        private $status, $json;
+
+        private $errors = [
+            0 => 'Undefined',
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            102 => 'Processing',
+            103 => 'Early Hints',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            207 => 'Multi-Status',
+            208 => 'Already Reported',
+            226 => 'IM Used',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => 'Switch Proxy',
+            307 => 'Temporary Redirect',
+            308 => 'Permanent Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized ',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Payload Too Large',
+            414 => 'URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            418 => 'I\'m a teapot ',
+            421 => 'Misdirected Request',
+            422 => 'Unprocessable Entity',
+            423 => 'Locked',
+            424 => 'Failed Dependency',
+            425 => 'Too Early',
+            426 => 'Upgrade Required',
+            428 => 'Precondition Required',
+            429 => 'Too Many Requests',
+            431 => 'Request Header Fields Too Large',
+            451 => 'Unavailable For Legal Reasons',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported',
+            506 => 'Variant Also Negotiates',
+            507 => 'Insufficient Storage',
+            508 => 'Loop Detected',
+            510 => 'Not Extended',
+            511 => 'Network Authentication Required',
+        ];
+
+        public function __construct(int $status, $json = true) {
+            $this->status = $status;
+            $this->json = $json;
+        }
+
+        public function display(String $msg = null) {
+            $status = 0;
+            $name = $this->errors[0];
+
+            if(isset($this->errors[$this->status])) {
+                $status = $this->status;
+                $name = $this->errors[$this->status];
+            }
+
+            $response = [
+                'httpstatus' => [
+                    'name' => $name
+                ],
+                'status' => $status
+            ];
+
+            if($msg) $response['httpstatus']['message'] = $msg;
+
+            http_response_code($this->status);
+            echo $this->json ? json_encode($response, true) : $response;
+            return;
+        }
+    }
