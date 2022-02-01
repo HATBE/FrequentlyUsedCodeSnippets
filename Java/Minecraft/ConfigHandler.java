d6f03e5fd039750bfcf9c277cdbd12415075c6ae
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2022-02-01 18:59:15 +0100

    Create ConfigHandler.java

---
+++ Java/Minecraft/ConfigHandler.java
@@ -0,0 +1,46 @@
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class ConfigHandler {
+	private Main main;
+	private FileConfiguration config;
+	
+	public ConfigHandler(Main main) {
+		this.main = main;
+		config = main.getConfig();
+	}
+	
+	public void addDefault(String path, Object obj) {
+			config.addDefault(path, obj);
+			config.options().copyDefaults(true);
+			main.saveConfig();
+	}
+		
+	public FileConfiguration getConfig() {
+		return config;
+	}
+	
+	public void save() {
+		main.saveConfig();
+	}
+	
+	public void set(String path, Object obj) {
+		config.set(path, obj);
+	}
+	
+	public void delete(String path) {
+		config.set(path, null);
+	}
+	
+	public String getString(String path) {
+		return config.getString(path);
+	}
+	
+	public Double getDouble(String path) {
+		return config.getDouble(path);
+	}
+	
+	public int getInt(String path) {
+		return config.getInt(path);
+	}
+		
+}
