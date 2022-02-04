6c68f7bb1a56fc925605a5613f86dd9484dee7aa
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2022-02-04 09:09:06 +0100

    Update MessagesFunction.sh

--- BASH/MessagesFunction.sh
+++ BASH/MessagesFunction.sh
@@ -1,21 +1,26 @@
 SILENT=false
 
-readonly F_RED=$(tput setaf 1)
-readonly F_GREEN=$(tput setaf 2)
-readonly F_YELLOW=$(tput setaf 3)
-readonly F_RESET=$(tput sgr0)
+readonly F_R=$(tput setaf 1) # red
+readonly F_G=$(tput setaf 2) # green
+readonly F_Y=$(tput setaf 3) # yellow
+readonly F_B=$(tput setaf 4) # blue
+readonly F_C=$(tput setaf 6) # cyan
+readonly F_X=$(tput sgr0) 
 
-messages() {
-    if $SILENT; then return; fi
+function __messages() { # __messages <type> <message>
     local MODE=$1
     shift
+    if $SILENT; then return; fi
     case "${MODE}" in
-        error) echo "${F_RED}[Error]: ${F_RESET}${@}!" >&2 ;;
-        success) echo "${F_GREEN}[Success]: ${F_RESET}${@}." ;;
-        info) echo "${F_BLUE}[Info]: ${F_RESET}${@}." ;;
-        warn) echo "${F_YELLOW}[Warn]: ${F_RESET}${@}!" ;;
-        *) echo "${@}." ;;
+        'error') echo "${F_R}[Error]:${F_X} ${@}!" >&2;; # display error & redirect to stderr
+        'success') echo "${F_G}[Success]:${F_X} ${@}.";; #display success
+        'info') echo "${F_B}[Info]:${F_X} ${@}.";; # display info
+        'warn') echo "${F_Y}[Warning]:${F_X} ${@}!";; # display warning
+        *) echo "${@}";; # display just the plain message
     esac
 }
 
-messages 'error' 'This is an error'
+__messages 'error' 'This is a error'
+__messages 'success' 'This is a success'
+__messages 'info' 'This is a info'
+__messages 'warn' 'This is a warning'
